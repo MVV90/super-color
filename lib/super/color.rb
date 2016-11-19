77d6790c96d5827c06fb97485cd97ca3f551e040
MVV90 <matt.vincek@vydia.com>
2016-11-18 21:40:57 -0500

    super-color setup

---
+++ lib/super/color.rb
@@ -0,0 +1,7 @@
+require "super/color/version"
+
+module Super
+  module Color
+    # Your code goes here...
+  end
+end
---
+++ lib/super/color/version.rb
@@ -0,0 +1,5 @@
+module Super
+  module Color
+    VERSION = "0.1.0"
+  end
+end
